a3143d95aaac0ca558ceda0b2f892cfaf76882ba
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-09-14 18:34:42 +0500

    Task1

---
+++ Tyuiu.SinitsinDV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.SinitsinDV.Sprint0.Task1.V0
+{
+    static class programm 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");           
+            string name = "Дима";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
