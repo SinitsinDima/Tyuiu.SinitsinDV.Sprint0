9fbd0668d1d4f319382561d32393b6f763d35612
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-09-15 22:11:38 +0500

    Task3

---
+++ Tyuiu.SinitsinDV.Sprint0.Task0.V0.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SinitsinDV.Sprint0.Task0.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SinitsinDV.Sprint0.Task3.Vo/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.SinitsinDV.Sprint0.Task3.V0.Lib; 
+namespace Tyuiu.SinitsinDV.Sprint0.Task3.Vo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
